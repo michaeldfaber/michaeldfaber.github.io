42faf3ef0b117ca15c6cc13e945e220f56822a1a
Mike Faber <mi3406@fabermmac-lt.assurity.local>
2021-04-23 11:33:06 -0500

    Updates

---
+++ precache-manifest.a9a5db4d6fd6c04968e1ee3fbe694843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2b0997b5266b496481182cfa65751e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb1aea27876a8b523150",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "13216a4ad2995814598a",
+    "url": "/static/js/2.ebf36b42.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ebf36b42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb1aea27876a8b523150",
+    "url": "/static/js/main.c36d3ffe.chunk.js"
+  },
+  {
+    "revision": "a913fa7f60370fe23693",
+    "url": "/static/js/runtime-main.9c743f3c.js"
+  }
+]);
