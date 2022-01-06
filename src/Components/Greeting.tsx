c447a7d2eae69cb61d8ead8dcdcda00b3a9d083c
Michael Faber <michaelfaber@Michaels-MacBook-Air.local>
2022-01-06 12:17:40 -0600

    greeting component

--- src/Components/Greeting.tsx
+++ src/Components/Greeting.tsx
@@ -5,16 +5,19 @@ import './Greeting.css'
 export class Greeting extends Component {
     render() {
         return (
-            <div className="greeting-container">
+            <div className="greeting">
+                <img className="greeting-photo" src={require('../Photos/me.jpg')} />
                 <div className="greeting-heading">
                     Greetings!
                 </div>
                 <div className="greeting-subheading">
                     My name is Michael Faber.
                 </div>
-                <Button />
-                <Button />
-                <Button />
+                <div className="greeting-body">
+                    <p>Software Engineer with over 5 years of experience.</p>
+                    <p>I love developing web applications, speeding up the process of developing web applications, and making web applications more secure.</p>
+                    <p>I created this simple website to showcase my career, blog, and personal projects.</p>
+                </div>
             </div>
         );
     }
