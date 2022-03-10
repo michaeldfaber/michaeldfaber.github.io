822463febe81ff9f8a356c2094bbce08ef45e113
Michael Faber <michaelf1444@gmail.com>
2022-03-10 15:43:41 -0600

    navbar looking good on desktop

--- src/Components/Layout/Navbar.tsx
+++ src/Components/Layout/Navbar.tsx
@@ -5,14 +5,25 @@ export class Navbar extends Component {
     render() {
         return (
             <div className="navbar">
-                <div className="navbar-link">
-                    <a href="/">Home</a>
-                </div>
-                <div className="navbar-link">
-                    <a href="https://www.linkedin.com/in/michaeldfaber/">LinkedIn</a>
-                </div>
-                <div className="navbar-link">
-                    <a href="https://michaeldfaber.medium.com/">Medium</a>
+                <div className="navbar-links-container">
+                    <div className="navbar-link">
+                        <a href="/">Home</a>
+                    </div>
+                    <div className="navbar-link">
+                        <a href="https://www.github.com/michaeldfaber/">Resume</a>
+                    </div>
+                    <div className="navbar-link">
+                        <a href="https://www.github.com/michaeldfaber/">Playground</a>
+                    </div>
+                    <div className="navbar-link">
+                        <a href="https://www.github.com/michaeldfaber/">GitHub</a>
+                    </div>
+                    <div className="navbar-link">
+                        <a href="https://www.linkedin.com/in/michaeldfaber/">LinkedIn</a>
+                    </div>
+                    <div className="navbar-link">
+                        <a href="https://michaeldfaber.medium.com/">Medium</a>
+                    </div>
                 </div>
             </div>
         );
