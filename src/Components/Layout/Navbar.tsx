59d0919435ec7ecb5717ff0c29ce3d48f7d705d2
Michael Faber <michaelf1444@gmail.com>
2022-03-10 19:26:08 -0600

    navbar looking good on mobile

--- src/Components/Layout/Navbar.tsx
+++ src/Components/Layout/Navbar.tsx
@@ -1,30 +1,66 @@
 import React, { Component } from 'react';
 import './../../Styles/Common.css';
 
-export class Navbar extends Component {
+type NavBarState = {
+    menuOpen?: boolean
+}
+
+export class Navbar extends Component<unknown, NavBarState> {
+    constructor(props: unknown) {
+        super(props);
+        this.state = {
+            menuOpen: true
+        }
+        this.handleMobileResize = this.handleMobileResize.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", () => this.handleMobileResize());
+    }
+
+    handleMobileResize() {
+        if(window.innerWidth >= 1201)
+        {
+            this.setState({ menuOpen: true }, this.forceUpdate);
+        }
+
+        if(window.innerWidth <= 1200)
+        {
+            this.setState({ menuOpen: false }, this.forceUpdate);
+        }
+    }
+
+    handleMobileToggle() {
+        const toggle = this.state.menuOpen;
+        this.setState({ menuOpen: !toggle }, this.forceUpdate);
+    }
+
     render() {
         return (
             <div className="navbar">
-                <div className="navbar-links-container">
-                    <div className="navbar-link">
-                        <a href="/">Home</a>
+                <div className="navbar-hamburger" onClick={() => this.handleMobileToggle()}>≡</div>
+                {this.state.menuOpen && (
+                    <div className="navbar-links-container">
+                        <div className="navbar-link">
+                            <a href="/">Home</a>
+                        </div>
+                        <div className="navbar-link">
+                            <a href="https://www.github.com/michaeldfaber/">Resume</a>
+                        </div>
+                        <div className="navbar-link">
+                            <a href="https://www.github.com/michaeldfaber/">Playground</a>
+                        </div>
+                        <div className="navbar-link">
+                            <a href="https://www.github.com/michaeldfaber/">GitHub</a>
+                        </div>
+                        <div className="navbar-link">
+                            <a href="https://www.linkedin.com/in/michaeldfaber/">LinkedIn</a>
+                        </div>
+                        <div className="navbar-link">
+                            <a href="https://michaeldfaber.medium.com/">Medium</a>
+                        </div>
                     </div>
-                    <div className="navbar-link">
-                        <a href="https://www.github.com/michaeldfaber/">Resume</a>
-                    </div>
-                    <div className="navbar-link">
-                        <a href="https://www.github.com/michaeldfaber/">Playground</a>
-                    </div>
-                    <div className="navbar-link">
-                        <a href="https://www.github.com/michaeldfaber/">GitHub</a>
-                    </div>
-                    <div className="navbar-link">
-                        <a href="https://www.linkedin.com/in/michaeldfaber/">LinkedIn</a>
-                    </div>
-                    <div className="navbar-link">
-                        <a href="https://michaeldfaber.medium.com/">Medium</a>
-                    </div>
-                </div>
+                )}
             </div>
         );
     }
